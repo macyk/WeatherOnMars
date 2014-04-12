642b0904364ac010e757de4f19023a3b74db4ec8
Macy Kuang <mkuang@woozworld.com>
2014-04-12 12:18:11 -0400

    * add activity and services for Mars weather

---
+++ src/com/google/android/glass/sample/compass/WeatherMenuActivity.java
@@ -0,0 +1,13 @@
+package com.google.android.glass.sample.compass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Macy on 2014-04-12.
+ */
+public class WeatherMenuActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/com/google/android/glass/sample/compass/WeatherServices.java
@@ -0,0 +1,14 @@
+package com.google.android.glass.sample.compass;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Macy on 2014-04-12.
+ */
+public class WeatherServices extends Service {
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

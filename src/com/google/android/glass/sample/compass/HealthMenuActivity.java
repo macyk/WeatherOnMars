e2769f2adfd5dfb39bbaf26911dc1a62a35bda81
Macy Kuang <mkuang@woozworld.com>
2014-04-12 12:24:29 -0400

    + add show health call

---
+++ src/com/google/android/glass/sample/compass/HealthMenuActivity.java
@@ -0,0 +1,13 @@
+package com.google.android.glass.sample.compass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Macy on 2014-04-12.
+ */
+public class HealthMenuActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

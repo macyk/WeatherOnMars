dbe01adba484337517595f1834965080d762079d
damien <dalien2611@hotmail.fr>
2014-04-12 13:29:38 -0400

    mars

--- src/com/google/android/glass/sample/compass/CompassService.java
+++ src/com/google/android/glass/sample/compass/CompassService.java
@@ -96,7 +96,6 @@ public class CompassService extends Service {
     public void onCreate() {
         super.onCreate();
 
-      new RequestTask().execute("http://marsweather.ingenology.com/v1/archive/?sol=155");
 
 
       mTimelineManager = TimelineManager.from(this);
@@ -161,43 +160,4 @@ public class CompassService extends Service {
         super.onDestroy();
     }
 
-  class RequestTask extends AsyncTask<String, String, String> {
-
-    @Override
-    protected String doInBackground(String... uri) {
-      HttpClient httpclient = new DefaultHttpClient();
-      HttpResponse response;
-      String responseString = null;
-      try {
-        response = httpclient.execute(new HttpGet(uri[0]));
-        StatusLine statusLine = response.getStatusLine();
-        if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-          ByteArrayOutputStream out = new ByteArrayOutputStream();
-          response.getEntity().writeTo(out);
-          out.close();
-          responseString = out.toString();
-        } else{
-          //Closes the connection.
-          response.getEntity().getContent().close();
-          throw new IOException(statusLine.getReasonPhrase());
-        }
-      } catch (ClientProtocolException e) {
-        //TODO Handle problems..
-      } catch (IOException e) {
-        //TODO Handle problems..
-      }
-      return responseString;
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-      super.onPostExecute(result);
-
-
-      Log.d("weather", result);
-
-
-      //Do anything with response..
-    }
-  }
 }
--- src/com/google/android/glass/sample/compass/WeatherServices.java
+++ src/com/google/android/glass/sample/compass/WeatherServices.java
@@ -2,7 +2,20 @@ package com.google.android.glass.sample.compass;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Macy on 2014-04-12.
@@ -11,4 +24,51 @@ public class WeatherServices extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    new RequestTask().execute("http://marsweather.ingenology.com/v1/archive/?sol=155");
+
+  }
+
+  class RequestTask extends AsyncTask<String, String, String> {
+
+    @Override
+    protected String doInBackground(String... uri) {
+      HttpClient httpclient = new DefaultHttpClient();
+      HttpResponse response;
+      String responseString = null;
+      try {
+        response = httpclient.execute(new HttpGet(uri[0]));
+        StatusLine statusLine = response.getStatusLine();
+        if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+          ByteArrayOutputStream out = new ByteArrayOutputStream();
+          response.getEntity().writeTo(out);
+          out.close();
+          responseString = out.toString();
+        } else{
+          //Closes the connection.
+          response.getEntity().getContent().close();
+          throw new IOException(statusLine.getReasonPhrase());
+        }
+      } catch (ClientProtocolException e) {
+        //TODO Handle problems..
+      } catch (IOException e) {
+        //TODO Handle problems..
+      }
+      return responseString;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+      super.onPostExecute(result);
+
+
+      Log.d("weather", result);
+
+
+      //Do anything with response..
+    }
+  }
 }

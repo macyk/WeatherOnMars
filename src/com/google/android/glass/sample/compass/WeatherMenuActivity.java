48a80fe101a9d48bebd3964508d6014ccb6b303e
Macy Kuang <mkuang@woozworld.com>
2014-04-12 13:53:23 -0400

    * show the compass for the weather

--- src/com/google/android/glass/sample/compass/WeatherMenuActivity.java
+++ src/com/google/android/glass/sample/compass/WeatherMenuActivity.java
@@ -1,13 +1,39 @@
 package com.google.android.glass.sample.compass;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 
 /**
  * Created by Macy on 2014-04-12.
  */
 public class WeatherMenuActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+    private WeatherServices.WeatherBinder mWeatherService;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            if (service instanceof WeatherServices.WeatherBinder) {
+                mWeatherService = (WeatherServices.WeatherBinder) service;
+                openOptionsMenu();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            // Do nothing.
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bindService(new Intent(this, CompassService.class), mConnection, 0);
+        Log.d("weather", "downloading weather");
+
+
+
     }
 }
--- src/com/google/android/glass/sample/compass/WeatherServices.java
+++ src/com/google/android/glass/sample/compass/WeatherServices.java
@@ -1,10 +1,22 @@
 package com.google.android.glass.sample.compass;
 
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.hardware.SensorManager;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Binder;
 import android.os.IBinder;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
+
+import com.google.android.glass.sample.compass.model.Landmarks;
+import com.google.android.glass.sample.compass.util.MathUtils;
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.TimelineManager;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -21,15 +33,75 @@ import java.io.IOException;
  * Created by Macy on 2014-04-12.
  */
 public class WeatherServices extends Service {
+    private static final String LIVE_CARD_ID = "weather";
+    private LiveCard mLiveCard;
+    private TimelineManager mTimelineManager;
+    private CompassRenderer mRenderer;
+    private Landmarks mLandmarks;
+    private OrientationManager mOrientationManager;
+    private TextToSpeech mSpeech;
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    /**
+     * A binder that gives other components access to the speech capabilities provided by the
+     * service.
+     */
+    public class WeatherBinder extends Binder {
+        /**
+         * Read the current heading aloud using the text-to-speech engine.
+         */
+        public void readHeadingAloud() {
+            float heading = mOrientationManager.getHeading();
+
+            Resources res = getResources();
+            String[] spokenDirections = res.getStringArray(R.array.spoken_directions);
+            String directionName = spokenDirections[MathUtils.getHalfWindIndex(heading)];
+
+            int roundedHeading = Math.round(heading);
+            int headingFormat;
+            if (roundedHeading == 1) {
+                headingFormat = R.string.spoken_heading_format_one;
+            } else {
+                headingFormat = R.string.spoken_heading_format;
+            }
+
+            String headingText = res.getString(headingFormat, roundedHeading, directionName);
+            mSpeech.speak(headingText, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mLiveCard == null) {
+            mLiveCard = mTimelineManager.createLiveCard(LIVE_CARD_ID);
+            mRenderer = new CompassRenderer(this, mOrientationManager, mLandmarks);
+
+            mLiveCard.setDirectRenderingEnabled(true).getSurfaceHolder().addCallback(mRenderer);
+
+            // Display the options menu when the live card is tapped.
+            Intent menuIntent = new Intent(this, CompassMenuActivity.class);
+            menuIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            mLiveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent, 0));
+
+            mLiveCard.publish(LiveCard.PublishMode.REVEAL);
+        }
+
+        return START_STICKY;
     }
 
   @Override
   public void onCreate() {
     super.onCreate();
+      SensorManager sensorManager =
+              (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+      LocationManager locationManager =
+              (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+      mOrientationManager = new OrientationManager(sensorManager, locationManager);
+      mLandmarks = new Landmarks(this);
+      mTimelineManager = TimelineManager.from(this);
     new RequestTask().execute("http://marsweather.ingenology.com/v1/archive/?sol=155");
-
   }
 
   class RequestTask extends AsyncTask<String, String, String> {
